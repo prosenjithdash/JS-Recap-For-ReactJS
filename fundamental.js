e0e63c80f319e50829d41fd3223f8780fd89bfaf
prosenjithdashpappu <prosenjith.edu@gmail.com>
2024-03-06 21:31:42 +0600

    Recap for react

---
+++ fundamental.js
@@ -0,0 +1,12 @@
+// 1. How to declare a variable using let and const
+
+
+// const
+const myName = 'Prosenjith Dash Pappu'; // No change variable name.
+console.log(myName);
+
+
+// let
+let session = 'Winter'
+session = 'Summer'
+console.log(session);
